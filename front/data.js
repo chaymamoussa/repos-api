210dbfdd84c1a385b85ed97f121b5523a6736a48
chaymamoussa <chayma.moussa@ensi-uma.tn>
2022-04-14 00:10:20 +0100

    add readme file and key.json

--- front/data.js
+++ front/data.js
@@ -1,19 +1,33 @@
-let results; 
+var labels, values;
 $.ajax({
     type: 'GET',
     url: 'http://127.0.0.1:5000/licence',
     success: function(response) {
-      results = response
+        labels = response.labels
+        values = response.data
     },
     async: false
-  });
+});
 
+var colors = [];
+var dynamicColors = function() {
+    var r = Math.floor(Math.random() * 255);
+    var g = Math.floor(Math.random() * 255);
+    var b = Math.floor(Math.random() * 255);
+    return "rgb(" + r + "," + g + "," + b + ")";
+};
+
+for (var i in values) {
+    colors.push(dynamicColors())
+}
+ 
 const data = {
-    labels: results.labels,
+    labels: labels,
     datasets: [
         {
             label: 'Licenses',
-            data: results.data,
+            data: values,
+            backgroundColor: colors
         },
     ],
 };
